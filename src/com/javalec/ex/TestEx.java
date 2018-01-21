923acafc72019e3da547a9e366424f2c4c2f2441
User <User@UseongSim.fritz.box>
2018-01-21 21:31:04 +0100

    initial commit

---
+++ src/com/javalec/ex/TestEx.java
@@ -0,0 +1,42 @@
+package com.javalec.ex;
+
+public class TestEx {
+	public static void main(String[] args) {
+		
+		int i=20;
+		int j=30;
+		int k=40;
+		
+		char c='A';
+		char d='B';
+		char e='C';
+		
+		double x=3.14D;
+		boolean b=true;
+		
+		System.out.println(x);
+		System.out.println(b);
+		
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+				
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+		
+			
+			int i1=10;
+			double d1=i1;
+			
+			System.out.println(d1);
+		
+			double d2=3.14D;
+			int i2=(int)d2;
+			
+			
+			System.out.println(i2);
+	}
+}
---
+++ src/com/javalec/ex/VarKind.java
@@ -0,0 +1,12 @@
+package com.javalec.ex;
+
+public class VarKind {
+
+	public static void name() {
+		
+		int i1=10;
+		double d1=i1;
+		
+		System.out.println(d1);
+	}
+}
